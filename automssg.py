9f6f844b466b11093f5e03343cc0d4b92d015b98
tanay513 <147440805+tanay513@users.noreply.github.com>
2025-08-12 21:44:25 +0530

    Add files via upload

---
+++ automssg.py
@@ -0,0 +1,17 @@
+import pyautogui as pg
+import time
+
+print("starting in 5 seconds....'")
+time.sleep(5)
+
+for i in range(1000):
+    pg.write("you cant sleep abuzarrr")
+    pg.press("enter")
+
+
+
+        
+
+
+
+
